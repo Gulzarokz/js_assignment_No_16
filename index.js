6ff72acdb91f952a79b902d0aa7e8f17804f69a0
Gulzarokz <gokz22301@gamil.com>
2024-11-21 18:16:55 +0500

    function with passing argument and parameter

---
+++ index.js
@@ -0,0 +1,80 @@
+// 35:Functions
+// What is function
+//functions is block of javascript that robatically the same thing again and again whenEver you invoked
+// means call it
+
+
+
+//why function needs
+//It saves your codes reptatively and make code easier to understand
+
+// const now = new Date();
+// const theHr = now.getHours();
+// const theMin = now.getMinutes();
+// const theSec = now.getSeconds();
+// console.log(`Time: ${theHr}: ${theMin}: ${theSec}`);
+
+
+// function tellTime(){
+// const now = new Date();
+// const theHr = now.getHours();
+// const theMin = now.getMinutes();
+// const theSec = now.getSeconds();
+// console.log(`Time: ${theHr}: ${theMin}: ${theSec}`);
+
+// }
+// tellTime();
+// tellTime();
+
+
+
+// function declareation
+// function hellworld(){
+//     alert("Hello, world!");
+// }
+
+// function calling
+// hellworld();
+
+// function greetTheUser(greetTheUser){
+//     console.log("Hello :" +greetTheUser); 
+    
+// }
+// greetTheUser("Gulzar");
+
+//36Functions: Passing them data
+
+// function printNumber(num){ // The data inside the parentheisis in a a function statment is known as a parameter
+//       alert(num)
+// }
+
+// //function calling
+// printNumber(10); //Arrguments which is passing during the execuation time
+
+
+// different data pass 
+//  function greetTheUser(greeting){
+//     alert(greeting);
+//  }
+//  greetTheUser("Hello");
+//  greetTheUser("Asalam o alikom");
+//  greetTheUser("How are you doing");
+
+// can pass as variables
+// function greetTheUser(greeting){
+
+//     alert(greeting);
+// }
+// const greetingMessages = "Hello";
+// greetTheUser(greetingMessages);
+
+// passing multiple Arrguments
+
+// function userDetails(userName, age, isMarried){
+//     console.log(`Your name is ${userName} your age is ${age} and your martian is ${isMarried}`);
+    
+// }
+
+//arrgument order matters here
+// userDetails("Gulzar khan", 40, true);
+
